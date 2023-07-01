d1ae73539a85ac5ba80589f133fbc2cb5525f1b8
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-07-01 15:59:33 +0530

    ✨ Game Dimentions Added

---
+++ app/GameDimensions.java
@@ -0,0 +1,14 @@
+package app;
+import java.awt.Dimension;
+
+public class GameDimensions {
+
+    public static final Dimension PANEL_SIZE = new Dimension(800, 600);
+    public static final int SNAKE_SIZE = 20;
+    public static final int FOOD_SIZE = 20;
+    public static final int WALL_WIDTH = 20;
+    public static final int WALL_HEIGHT = 20;
+    
+
+}
+
